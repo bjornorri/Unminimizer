e955a7519c74dd912a852146d4c58c3d4c6e6280
Björn Saemundsson <bjornorri@gmail.com>
2025-11-20 19:40:21 +0100

    Initial Commit

---
+++ UnminimizerTests/UnminimizerTests.swift
@@ -0,0 +1,17 @@
+//
+//  UnminimizerTests.swift
+//  UnminimizerTests
+//
+//  Created by Bjorn Orri Saemundsson on 20.11.2025.
+//
+
+import Testing
+@testable import Unminimizer
+
+struct UnminimizerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
